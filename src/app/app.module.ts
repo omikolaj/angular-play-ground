29b66098764de58e527cf07b3c9ca5a83026b4ed
Oskar Mikolajczyk <omikolaj1@gmail.com>
2019-10-29 18:47:31 -0400

    Basic sekelton for the playground app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoShellComponent } from './components/todo-shell/todo-shell.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoListComponent,
+    TodoShellComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
